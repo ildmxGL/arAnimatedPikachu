6a1f8d6db0c61883e15bd2d6c957a7a45018c656
ildmxGL <ildmx.GL@gmail.com>
2019-01-30 17:52:36 +0900

    Add unity project

---
+++ Assets/Resources/animController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("Default Take"))
+            {
+                anim.Play("begin");
+            }
+            else if (touch.phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("begin"))
+            {
+                anim.Play("begin");
+            }
+        }
+        */
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("Default Take"))
+            {
+                anim.Play("bow");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("begin"))
+            {
+                anim.Play("bow");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("bow"))
+            {
+                anim.Play("gauddung");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("gauddung"))
+            {
+                anim.Play("doridori");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("doridori"))
+            {
+                anim.Play("bow");
+            }
+        }
+    }
+}
---
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,166 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    public Animator anim;
+
+    // Begin Sound
+    public AudioSource soundTarget;
+    public AudioClip clipTarget;
+    private AudioSource[] allAudioSources;
+
+    void StopAllAudio()
+    {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+        }
+    }
+
+    void playSound(string ss)
+    {
+        clipTarget = (AudioClip)Resources.Load(ss);
+        soundTarget.clip = clipTarget;
+        soundTarget.loop = true;
+        soundTarget.playOnAwake = false;
+        soundTarget.Play();
+    }
+
+    // End Sound
+
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        anim = GetComponent<Animator>();
+        soundTarget = (AudioSource)gameObject.AddComponent<AudioSource>();
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+
+        // Play Sound, if detect an target
+        playSound("sounds/bgm");
+
+        anim.Play("begin");
+
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+
+        StopAllAudio();
+        // anim.StopPlayback();
+
+    }
+
+    #endregion // PROTECTED_METHODS
+}
